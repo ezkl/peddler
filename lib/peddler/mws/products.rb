4667351c92968743484f0d1c68786339061100d4
Hakan Ensari <hakan.ensari@papercavalier.com>
2012-07-10 17:28:11 +0100

    Clean up internals of Products

--- lib/peddler/mws/products.rb
+++ lib/peddler/mws/products.rb
@@ -1,71 +1,33 @@
-require 'forwardable'
 
 require 'peddler/api'
 
 module Peddler
   module MWS
     # A Client to the Amazon MWS Products API.
-      def get_competitive_pricing_for_asin
-        raise NotImplementedError
-      end
     class Products
       include API
 
-      def get_competitive_pricing_for_sku
+      def get_competitive_pricing
         raise NotImplementedError
       end
 
-      # Gets the lowest price offer listings for a product.
-      #
-      # asins     - A String or Array list of up to 20 ASINs.
-      # condition - The String item condition scope (default: nil)
-      #
-      # Returns a Peddler::Response.
-      def get_lowest_offer_listings_for_asin(asins, condition = nil)
-        params = { 'Action' => 'GetLowestOfferListingsForASIN' }
-        params.update list 'ASIN', asins
-        params.store 'ItemCondition', condition if condition
-
-        Response.new get query: params
-      end
-
-      def get_lowest_offer_listings_for_sku
+      def get_lowest_offers
         raise NotImplementedError
       end
 
-      # Gets a list of products and their attributes.
-      #
-      # asins - A String or Array list of up to 10 ASINs.
-      #
-      # Returns a MWS::Response.
-      #
-      # Raises a BadResponse if response is not valid.
-      def get_matching_product(asins)
-        params = build_list 'ASIN', asins
-        url = products_url 'GetMatchingProduct', params
-
-        res = Response.new connection.get url
-
-        if res.valid?
-          res
-        else
-          raise BadResponse, res.first('Error')['Message']
-        end
-      end
-
-      def get_my_price_for_asin
+      def get_matching
         raise NotImplementedError
       end
 
-      def get_my_price_for_sku
+      def get_my_price
         raise NotImplementedError
       end
 
-      def get_product_categories_for_asin
+      def get_categories
         raise NotImplementedError
       end
 
-      def get_product_categories_for_sku
+      def list_matching
         raise NotImplementedError
       end
 
@@ -81,8 +43,6 @@ module Peddler
         end
       end
 
-      def list_matching_products
-        raise NotImplementedError
       end
     end
   end
