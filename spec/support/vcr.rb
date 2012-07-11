48a6c6fe2c9755879a50a40f5cd63063d7d21d92
Hakan Ensari <hakan.ensari@papercavalier.com>
2012-07-11 19:34:15 +0100

    Add an AWS request matcher for VCR

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -4,9 +4,17 @@ RSpec.configure do |c|
   c.treat_symbols_as_metadata_keys_with_true_values = true
 end
 
+aws_uri_matcher = VCR
+  .request_matchers
+  .uri_without_params 'Timestamp',
+                      'AWSAccessKeyId',
+                      'AssociateTag',
+                      'Signature'
+
 VCR.configure do |c|
-  c.cassette_library_dir = 'fixtures'
+  c.cassette_library_dir = 'spec/fixtures'
   c.configure_rspec_metadata!
-  c.default_cassette_options = { record: :new_episodes }
+  c.default_cassette_options = { record:            :new_episodes,
+                                 match_requests_on: [aws_uri_matcher] }
   c.hook_into :excon
 end
